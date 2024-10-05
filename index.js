6ed5e5991bee324f5048d5b1bc79a7119b805946
arsh1476 <149099680+arsh1476@users.noreply.github.com>
2024-10-05 13:39:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { initializeApp } from 'firebase/app';
+
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
